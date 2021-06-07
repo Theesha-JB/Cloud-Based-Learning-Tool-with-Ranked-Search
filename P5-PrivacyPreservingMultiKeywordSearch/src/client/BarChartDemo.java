ec3c88185a8e256dff71cc29e38bff52a4e8497f
Theesha JB <theeshajb@gmail.com>
2021-06-07 12:56:57 +0530

    first commit

---
+++ P5-PrivacyPreservingMultiKeywordSearch/src/client/BarChartDemo.java
@@ -0,0 +1,170 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+/**
+ *
+ * @author 
+ */
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.util.ArrayList;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+/**
+ * A simple demonstration application showing how to create a bar chart.
+ *
+ */
+public class BarChartDemo extends ApplicationFrame {
+    public static ArrayList opbn =receivedata.opbn;
+    /**
+     * Creates a new demo instance.
+     *
+     * @param title  the frame title.
+     */
+    public BarChartDemo(final String title) {
+
+        super(title);
+
+        final CategoryDataset dataset = createDataset();
+        final JFreeChart chart = createChart(dataset);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(500, 270));
+        setContentPane(chartPanel);
+
+    }
+
+    /**
+     * Returns a sample dataset.
+     * 
+     * @return The dataset.
+     */
+    private CategoryDataset createDataset() {
+        
+        // row keys...
+        final String series1 = "MRSE";
+
+        // column keys...
+        final String category1 = "Overall Data";
+        final String category2 = "Retrieved Data";
+
+        // create the dataset...
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(1000, series1, category1);
+        dataset.addValue(opbn.size(), series1, category2);
+        
+        return dataset;
+        
+    }
+    
+    /**
+     * Creates a sample chart.
+     * 
+     * @param dataset  the dataset.
+     * 
+     * @return The chart.
+     */
+    private JFreeChart createChart(final CategoryDataset dataset) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createBarChart(
+            "",         // chart title
+            "Category",               // domain axis label
+            "Value",                  // range axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL, // orientation
+            true,                     // include legend
+            true,                     // tooltips?
+            false                     // URLs?
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+        // set the background color for the chart...
+        chart.setBackgroundPaint(Color.white);
+
+        // get a reference to the plot for further customisation...
+        final CategoryPlot plot = chart.getCategoryPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+
+        // set the range axis to display integers only...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        // disable bar outlines...
+        final BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setDrawBarOutline(false);
+        renderer.setMaximumBarWidth(.15);
+        
+        // set up gradient paints for series...
+        final GradientPaint gp0 = new GradientPaint(
+            0.0f, 0.0f, Color.blue, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp1 = new GradientPaint(
+            0.0f, 0.0f, Color.green, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp2 = new GradientPaint(
+            0.0f, 0.0f, Color.red, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        renderer.setSeriesPaint(0, gp0);
+        renderer.setSeriesPaint(1, gp1);
+        renderer.setSeriesPaint(2, gp2);
+
+        final CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setCategoryLabelPositions(
+            CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0)
+        );
+        // OPTIONAL CUSTOMISATION COMPLETED.
+        
+        return chart;
+        
+    }
+    
+    // ****************************************************************************
+    // * JFREECHART DEVELOPER GUIDE                                               *
+    // * The JFreeChart Developer Guide, written by David Gilbert, is available   *
+    // * to purchase from Object Refinery Limited:                                *
+    // *                                                                          *
+    // * http://www.object-refinery.com/jfreechart/guide.html                     *
+    // *                                                                          *
+    // * Sales are used to provide funding for the JFreeChart project - please    * 
+    // * support us so that we can continue developing free software.             *
+    // ****************************************************************************
+    
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void main(final String[] args) {
+
+        final BarChartDemo demo = new BarChartDemo("");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+    }
+
+}
---
+++ P5-PrivacyPreservingMultiKeywordSearch/src/privacypreserving/AESencrp.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privacypreserving;
+import java.lang.String.*;
+import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.*;
+import java.util.Base64;
+/**
+ *encryptedData=Base64.getEncoder().encodeToString(encryptedByteArray);
+ * @author 
+ */
+class AESencrp {
+    private static final String ALGO = "AES";
+    public static String encrypt(String Data, String k) throws Exception {
+        Key key = generateKey(k);
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(Data.getBytes());
+      //  String encryptedValue = new BASE64Encoder().encode(encVal);
+       // String encryptedValue=Base64.getEncoder().encodeToString(encVal);
+        String encryptedValue=Base64.getEncoder().encodeToString(encVal);
+        return encryptedValue;
+    }
+    public static String decrypt(String encryptedData, String k) throws Exception {
+        Key key = generateKey(k);
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE, key);
+      //  byte[] decordedValue = Base64.getDecoder().decode(encryptedData);
+        byte[] decordedValue = Base64.getDecoder().decode(encryptedData);
+       // byte[] decordedValue =new BASE64Decoder().decodeBuffer(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        String decryptedValue = new String(decValue);
+        return decryptedValue;
+    }
+    private static Key generateKey(String k1) throws Exception {
+        //String k="muthuvelprograme";
+        byte[] keyValue=k1.getBytes();
+        Key key = new SecretKeySpec(keyValue, ALGO);
+        return key;
+    }
+}
